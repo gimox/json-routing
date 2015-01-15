5f74263578875b2396d6137914cc79fdf8ebee11
Giorgio Modoni <modogio@gmail.com>
2015-01-15 21:32:50 +0100

    more robust global options

--- test.js
+++ test.js
@@ -56,48 +56,9 @@ it('policy array test', function (done) {
 });
 
 it('test global', function (done) {
-    request.get(URL + '/testglobal', function (err, res, body) {
+    request.get(URL + '/demo/testglobal', function (err, res, body) {
         should.not.exist(err);
         res.statusCode.should.eql(200, 'status code is not 200');
         done();
     });
 });
-
-describe('simple routing', function () {
-
-
-    describe('index GET', function () {
-
-        it('GET must respond with 200', function (done) {
-            request.get(URL, function (err, res, body) {
-                should.not.exist(err);
-                res.statusCode.should.eql(200, 'status code is not 200');
-                done();
-            });
-        });
-    });
-
-    describe('index GET', function () {
-
-        it('POST must respond with 200', function (done) {
-            request.get(URL, function (err, res, body) {
-                should.not.exist(err);
-                res.statusCode.should.eql(200, 'status code is not 200');
-                done();
-            });
-        });
-    });
-
-    describe('this route can not exist', function () {
-        it('it must return 404', function (done) {
-            request.get(URL + "/notexist", function (err, res, body) {
-                should.not.exist(err);
-                res.statusCode.should.eql(404, 'wrong status code returned from server');
-                done();
-            });
-        });
-
-    });
-
-});
-
